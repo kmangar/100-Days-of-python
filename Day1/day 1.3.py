1deeee9bb12ed44986ed0736e1ab38b81749b105
kmangar <kham.mangar@outlook.com>
2022-01-04 14:11:07 -0500

    day 1.3
    input and len

---
+++ Day1/day 1.3.py
@@ -0,0 +1,5 @@
+# Write your code below this line 👇
+# Day 1.3
+# Write a program that prints the number of characters in a user's name.
+# You might need to Google for a function that calculates the length of a string.
+print(len(input("what is your name?")))
