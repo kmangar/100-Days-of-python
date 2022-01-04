344f47ec3dfa0456cdd11f74e54eddb2ba800189
kmangar <kham.mangar@outlook.com>
2022-01-04 13:49:56 -0500

    Initial commit

---
+++ Day1/day1.1.py
@@ -0,0 +1,14 @@
+#Write your code below this line 👇
+#Day 1.1
+print("Day 1 - Python Print Function");
+print('The function is declared like this:');
+print("print('what to print')")
+
+
+
+
+
+
+
+
+
