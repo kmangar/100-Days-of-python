2282b6c8adf1805a93ce4b3438312891cbd02663
kmangar <kham.mangar@outlook.com>
2022-01-04 14:00:21 -0500

    day1.2
    syntax error fix practice

--- Day1/day1.1.py
+++ Day1/day1.1.py
@@ -1,14 +1,5 @@
-#Write your code below this line 👇
-#Day 1.1
+# Write your code below this line 👇
+# Day 1.1
 print("Day 1 - Python Print Function");
 print('The function is declared like this:');
 print("print('what to print')")
-
-
-
-
-
-
-
-
-
