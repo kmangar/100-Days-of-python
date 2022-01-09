0ba98818af0da6b438a21dfee5aa06eff699bab7
kmangar <kham.mangar@outlook.com>
2022-01-09 17:03:42 -0500

    day 6
    reeborg's world

---
+++ Day6/Reeborg'sWorld.py
@@ -0,0 +1,52 @@
+# Hurdle 4
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+
+
+while not at_goal():
+    if wall_in_front:
+        jump()
+    else:
+        move()
+
+# Maze
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+while not at_goal():
+    if front_is_clear():
+        move()
+        if right_is_clear():
+            turn_right()
+            move()
+    elif right_is_clear():
+            turn_right()
+            move()
+    else:
+        turn_left()
