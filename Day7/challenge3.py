91f49b3a7298515052f24b234b3927bae9b81201
kmangar <kham.mangar@outlook.com>
2022-01-13 13:33:47 -0500

    update

--- Day7/challenge3.py
+++ Day7/challenge3.py
@@ -23,17 +23,19 @@ for x in chosen_word:
 print(temp)
 
 # Check guessed letter
+end_of_game = False
 
-while "_" in display:
+while not end_of_game:
     guess = input("Guess a letter: ").lower()
     for position in range(word_length):
         letter = chosen_word[position]
         #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             display[position] = letter
-            print(display)
+    print(display)
 
-if display == temp:
-    print("You win")
-else:
-    print("You Loose")
+    if "_" not in display:
+        print("You win")
+        end_of_game = True
+
+
