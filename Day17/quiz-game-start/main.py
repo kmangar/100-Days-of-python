e2b044247fc74cb34c7103e227bb36fcefd356f9
kmangar <kham.mangar@outlook.com>
2022-01-26 17:27:55 -0500

    Update main.py
    edited so it works and accepts the new data

--- Day17/quiz-game-start/main.py
+++ Day17/quiz-game-start/main.py
@@ -1,13 +1,12 @@
-from data import question_data
+from data import question_data, computer_easy
 from question_model import Question
 from quiz_brain import QuizBrain
 
 question_bank = []
 # Question(question_data)
 
-
-for question in question_data:
-    question_bank.append(Question(question['text'], question['answer']))
+for question in computer_easy:
+    question_bank.append(Question(question['question'], question['correct_answer']))
     # print(question_bank)
 
 quiz = QuizBrain(question_bank)
@@ -15,3 +14,7 @@ quiz = QuizBrain(question_bank)
 
 while quiz.still_has_question():
     quiz.next_question()
+
+print("You've Completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
