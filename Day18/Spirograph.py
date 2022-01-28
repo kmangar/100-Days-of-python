d64b67dd4e44dfc37dcae902891cc304039c3a5e
kmangar <kham.mangar@outlook.com>
2022-01-27 22:01:41 -0500

    Update Spirograph.py

--- Day18/Spirograph.py
+++ Day18/Spirograph.py
@@ -15,11 +15,14 @@ def random_color():
     b = random.randint(0, 255)
     return (r, g, b)
 
-john.speed(0)
 
-for _ in range(90):
-    john.color(random_color())
-    john.circle(100)
-    john.right(7)
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360/size_of_gap)):
+        john.color(random_color())
+        john.circle(100)
+        john.setheading(john.heading() + size_of_gap)
+
+john.speed(0)
+draw_spirograph(7)
 
 screen.exitonclick()
