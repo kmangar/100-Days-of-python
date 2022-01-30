30c2b60ebf6d24f57c411644b4f757987223c88a
kmangar <kham.mangar@outlook.com>
2022-01-30 17:49:22 -0500

    Update snake.py

--- Day20/snake_game/snake.py
+++ Day20/snake_game/snake.py
@@ -1,12 +1,21 @@
 from turtle import Turtle
+# INITIALIZE CONSTANT VARIABLE
 STARTING_POS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
-class Snake:
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
+
+class Snake:
+    # CONSTRUCTOR
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
+    # CREATES A SNAKE INSTANCE AND PUTS IT IN SCREEN
     def create_snake(self):
 
         for position in STARTING_POS:
@@ -16,22 +25,31 @@ class Snake:
             new_segment.goto(position)
             self.segments.append(new_segment)
 
+    # MOVES THE SNAKE, moves the last piece forward to the second last pos and so on
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
 
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
+    # GO UP IF NOT GOING DOWN
     def up(self):
-        self.segments[0].setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
+    # GO DOWN IF NOT GOING UP
     def down(self):
-        self.segments[0].setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
+    # GO LEFT IF NOT GOING RIGHT
     def left(self):
-        self.segments[0].setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
 
+    # GO RIGHT IF NOT GOING LEFT
     def right(self):
-        self.segments[0].setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
