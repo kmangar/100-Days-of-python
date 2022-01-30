1bcfbad59a02c90af59aa81bbe1078ec905598c7
kmangar <kham.mangar@outlook.com>
2022-01-30 17:49:38 -0500

    Create scoreboard.py

---
+++ Day20/snake_game/scoreboard.py
@@ -0,0 +1,23 @@
+from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ("Courier", 16, "bold")
+
+
+class Score(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.score = 0
+        self.penup()
+        self.color("white")
+        self.goto(-5, 275)
+        self.score_update()
+
+    def score_update(self):
+        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
+
+    def keeper(self):
+        self.score += 1
+        self.clear()
+        self.score_update()
