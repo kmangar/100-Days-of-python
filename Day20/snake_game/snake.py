e461c164bee883f7a374ce7bd8b4471aeb32c426
kmangar <kham.mangar@outlook.com>
2022-01-30 20:06:33 -0500

    Update snake.py
    extend functionality to add when snake eats food

--- Day20/snake_game/snake.py
+++ Day20/snake_game/snake.py
@@ -19,11 +19,17 @@ class Snake:
     def create_snake(self):
 
         for position in STARTING_POS:
-            new_segment = Turtle(shape="square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+        
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     # MOVES THE SNAKE, moves the last piece forward to the second last pos and so on
     def move(self):
