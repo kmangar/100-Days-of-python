c982eb19568062252c72254a4bc1adf449df0e88
Kham Mangar <kham.mangar@outlook.com>
2022-02-01 23:41:12 -0500

    player class with move and respawn function

--- Day23/TurtleCrossing/player.py
+++ Day23/TurtleCrossing/player.py
@@ -1,7 +1,22 @@
+from turtle import Turtle
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.respawn()
+
+    def respawn(self):
+        self.penup()
+        self.shape("turtle")
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+
+    def move(self):
+        if self.ycor() != 280:
+            self.forward(MOVE_DISTANCE)
+
