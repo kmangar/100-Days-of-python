afc06dc925711880f93c0b421f0e426f7699ef79
kmangar <kham.mangar@outlook.com>
2022-02-02 16:55:08 -0500

    Update main.py

--- Day24/Mail Merge Project Start/main.py
+++ Day24/Mail Merge Project Start/main.py
@@ -7,18 +7,19 @@
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
+PLACEHOLDER = "[name]"
 
 with open("./Input/Names/invited_names.txt") as data:
     datas = data.read()
     datas = datas.strip()
 
-names = datas.split('\n')
+names = datas
 
 with open("./Input/Letters/starting_letter.txt") as data:
     letter = data.read()
 
 for name in names:
-        new_letter = letter.replace("[name]", name)
-        with open(f"./Output/ReadyToSend/{name}.txt", mode='w') as file:
+        new_letter = letter.replace(PLACEHOLDER, name)
+        with open(f"./Output/ReadyToSend/letter_to_{name}.txt", mode='w') as file:
                 file.write(new_letter)
 
