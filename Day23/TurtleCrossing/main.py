d70fe3957044f9f5b9fe249d36ce89e9eb174e23
Kham Mangar <kham.mangar@outlook.com>
2022-02-02 00:37:13 -0500

    Update main.py

--- Day23/TurtleCrossing/main.py
+++ Day23/TurtleCrossing/main.py
@@ -9,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.move, "w")
@@ -19,5 +20,11 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    # When the turtle hits the top edge of the screen,
+    # it moves back to the original position and the player levels up. On the next level, the car speed increases.
     if player.ycor() == 280:
         player.respawn()
+
+    car_manager.new_car()
+    car_manager.car_move()
