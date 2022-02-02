90ca17e6fa35bca435ef0ff235223b1dcd2f16a0
kmangar <kham.mangar@outlook.com>
2022-02-02 15:14:25 -0500

    Update car_manager.py

--- Day23/TurtleCrossing/car_manager.py
+++ Day23/TurtleCrossing/car_manager.py
@@ -4,7 +4,7 @@ from turtle import Turtle
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
-
+STARTING_CARS = 5
 
 class CarManager():
 
@@ -12,24 +12,33 @@ class CarManager():
 
     def __init__(self):
         self.all_car = []
+        self.car_speed = STARTING_MOVE_DISTANCE
+        self.amount_of_cars = STARTING_CARS
+
+    def speed_up(self):
+        self.car_speed += MOVE_INCREMENT
+
+    def add_cars(self):
+        self.amount_of_cars += 1
 
     def new_car(self):
-        chance = random.randint(1, 6)
+        # chance = random.randint(1, 6)
 
-        if chance == 1:
-
-            new_car = Turtle()
-            new_car.penup()
-            new_car.shape("square")
-            new_car.speed(5)
-            new_car.goto(300, random.randint(-245, 245))
-            new_car.color(random.choice(COLORS))
-            new_car.shapesize(stretch_wid=1, stretch_len=2)
-            new_car.setheading(180)
-            # self.car_move()
-            self.all_car.append(new_car)
+        check = random.randint(1, 6)
+        for car in self.all_car:
+            if car.xcor() < -300:
+                car.goto(300, random.randrange(-245, 245))
+        if check == 1 and len(self.all_car) < self.amount_of_cars:
+            next_car = Turtle()
+            next_car.penup()
+            next_car.color(random.choice(COLORS))
+            next_car.shape("square")
+            next_car.shapesize(stretch_len=2, stretch_wid=1)
+            next_car.setheading(180)
+            next_car.goto(300, random.randrange(-245, 245))
+            self.all_car.append(next_car)
 
     def car_move(self):
-
         for car in self.all_car:
-                car.forward(STARTING_MOVE_DISTANCE)
+            car.forward(self.car_speed)
+
