9c1a88b6721cf8e4a25c95b1645c01ab6ce05769
Kham Mangar <kham.mangar@outlook.com>
2022-02-01 23:46:54 -0500

    Update main.py

--- Day23/TurtleCrossing/main.py
+++ Day23/TurtleCrossing/main.py
@@ -8,7 +8,16 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+
+screen.listen()
+screen.onkey(player.move, "w")
+screen.onkey(player.move, "Up")
+
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    if player.ycor() == 280:
+        player.respawn()
