0ac3f28a366013c5e579efe7e5c818b23066387b
kmangar <kham.mangar@outlook.com>
2022-02-03 16:26:55 -0500

    Create squirrel_count.py

---
+++ Day25/squirrel_count.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperature = []
+#     for row in data:
+#         if row[1] != "temp":
+#             temperature.append(int(row[1]))
+
+# data = pd.read_csv("weather_data.csv")
+
+# average = sum(data["temp"])/len(data["temp"])
+#
+# print(data["temp"].max())
+#
+# print(data[data.temp == data["temp"].max()])
+
+# monday = data[data.day == "Monday"]
+# monday_temp = int(monday.temp)
+# monday_temp = monday_temp *(9/5)+32
+# print(monday_temp)
+#
+
+# MAKE A CSV FILE WITH ALL THE COLORS AND COUNT MANY THERE ARE
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+gray_squirrel = data[data["Primary Fur Color"] == "Gray"]
+cinnamon_squirrel = data[data["Primary Fur Color"] == "Cinnamon"]
+black_squirrel = data[data["Primary Fur Color"] == "Black"]
+
+data_dict= {
+    "Fur Color": ["Grey", "Black", "Cinnamon"],
+    "Count": [len(gray_squirrel), len(black_squirrel), len(cinnamon_squirrel)]
+}
+
+df = pd.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
