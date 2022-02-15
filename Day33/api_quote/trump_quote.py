309e470540b03ffbe64ff1590c017392c3743487
Kham Mangar <kham.mangar@outlook.com>
2022-02-14 20:22:47 -0500

    img path change
    img path change

--- Day33/api_quote/trump_quote.py
+++ Day33/api_quote/trump_quote.py
@@ -15,13 +15,13 @@ window.title("Trump Says...")
 window.config(padx=50, pady=50)
 
 canvas = Canvas(width=300, height=414)
-background_img = PhotoImage(file="background.png")
+background_img = PhotoImage(file="img/background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Click trump Get A Quote", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Click trump Get A Quote", width=250, font=("Arial", 28, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 # image credit: https://pngimg.com/image/29355
-trump_img = PhotoImage(file="donald_trump.png")
+trump_img = PhotoImage(file="img/donald_trump.png")
 trump_button = Button(image=trump_img, highlightthickness=0, command=get_quote)
 trump_button.grid(row=1, column=0)
 
