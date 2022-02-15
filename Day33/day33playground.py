f59092a4db2bcda549e76be35e509bc2b39cd918
Kham Mangar <kham.mangar@outlook.com>
2022-02-14 20:45:26 -0500

    Update day33playground.py
    test area

--- Day33/day33playground.py
+++ Day33/day33playground.py
@@ -14,12 +14,36 @@
 # 
 # print(iss_position)
 
+# import requests
+#
+# response = requests.get("https://api.kanye.rest/")
+#
+# response.raise_for_status()
+#
+# quote = response.json()["quote"]
+#
+# print(quote)
+
 import requests
 
-response = requests.get("https://api.kanye.rest/")
+# ?lat=36.7201600&lng=-4.4203400
+MY_LAT = 38.192441
+MY_LNG = -85.716705
 
+parameters ={
+    "lat": MY_LAT,
+    "lng": MY_LNG,
+    "formatted": 0
+}
+
+
+response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 
-quote = response.json()["quote"]
+data = response.json()
+sunrise = data["results"]["sunrise"]
+sunset = data["results"]["sunset"]
 
-print(quote)
+print(sunrise)
+
+
