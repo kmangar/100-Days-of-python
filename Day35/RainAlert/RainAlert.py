a4429dda3e6499bbadd324cb97b19b5f7f2c2981
Kham Mangar <kham.mangar@outlook.com>
2022-02-22 22:57:43 -0500

    Create RainAlert.py

---
+++ Day35/RainAlert/RainAlert.py
@@ -0,0 +1,49 @@
+import requests
+import os
+from twilio.rest import Client
+
+
+account_sid = os.environ["ACCOUNT_SID"]
+auth_token = os.environ["AUTH_TOKEN"]
+
+api_key = os.environ["API_KEY"]
+
+# Below is coordinate for louisville, KY. HOWEVER, you can go to
+# https://www.latlong.net/ and get your city coordinate 😁
+latitude = 38.252666
+longitude = -85.758453
+
+# Para
+parameters = {
+    "lat": latitude,
+    "lon": longitude,
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
+}
+
+response = requests.get("https://api.openweathermap.org/data/2.5/onecall?", params=parameters)
+response.raise_for_status()
+
+weather_info = response.json()
+
+weather_today = weather_info["hourly"][:12]
+
+will_rain = False
+
+for hour_data in weather_today:
+    condition_code = (hour_data["weather"][0]["id"])
+    if condition_code < 600:
+        will_rain = True
+if will_rain:
+
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="\n\nIt's going to rain today Master. Remember to bring an ☔️ \n-🤵",
+        from_='+YOUR_TWILIO_NUMBER',
+        to='+YOUR_PHONE_NUMBER'
+    )
+    print(message.status)
+
+
+# print(weather_info["hourly"][0]["weather"][0]["id"])
