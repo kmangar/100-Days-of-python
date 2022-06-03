57cebb98001e2a7e408f783a32bb5cdcd117ff63
kmangar <kham.mangar@outlook.com>
2022-06-02 22:52:32 -0400

    Update main.py

--- Day61 Flask-WTF/main.py
+++ Day61 Flask-WTF/main.py
@@ -1,15 +1,26 @@
 from flask import Flask, render_template, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField
 
 app = Flask(__name__)
+
+app.secret_key = "some secret string"
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email')
+    password = StringField('Password')
 
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
+
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
 
 
 if __name__ == '__main__':
