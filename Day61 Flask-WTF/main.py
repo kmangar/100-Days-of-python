e29f7bd119c704dfdc1a7cc7fccce273892af5e2
kmangar <kham.mangar@outlook.com>
2022-06-07 22:16:03 -0400

    Update main.py
    code improvements

--- Day61 Flask-WTF/main.py
+++ Day61 Flask-WTF/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 
 app = Flask(__name__)
 
@@ -8,8 +8,9 @@ app.secret_key = "some secret string"
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email')
-    password = PasswordField('Password')
+    email = StringField(label='Email')
+    password = PasswordField(label='Password')
+    submit = SubmitField(label='Log in')
 
 
 @app.route("/")
